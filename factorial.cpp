99496a50c3428c19b15dba51f3677be025bafc7f
Talha Ahmad <talha.code92@gmail.com>
2025-01-22 18:59:20 -0800

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main()
 {
 	double num, fact = 1;
-	cout << "Enter number>>";
+	cout << "Enter number>> ";
 	cin >> num;
 	for (int n = 1; n <= num; n++)
 		fact *= n;
-	cout << "Factorial>>" << fact << endl;	
+	cout << "Factorial>> " << fact << endl;	
 	return 0;
 }
