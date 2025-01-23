1ef3ce36b29975a9998c22b3449d71cf0ac6b139
Talha Ahmad <talha.code92@gmail.com>
2025-01-22 19:18:51 -0800

    Add files via upload

---
+++ bank account.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+class BankAccount{
+	int number = 0;
+private:
+	int accountNumber;
+	string accountHolderName;
+	double balance;
+public:
+	BankAccount(string accountHolderName);
+	void deposit(double amount);
+	double withdraw(double amount);
+	void displayAccountDetails();
+};
+
+int main()
+{
+	BankAccount account("Talha Ahmad");
+	
+	account.deposit(50000);
+	account.withdraw(1000);
+	
+	account.displayAccountDetails();
+}
+
+BankAccount::BankAccount(string accountHolderName)
+{
+	this->accountHolderName = accountHolderName;
+	balance = 0;
+	accountNumber = ++number;
+}
+
+void BankAccount::deposit(double amount)
+{
+	balance += amount;
+}
+
+double BankAccount::withdraw(double amount)
+{
+	if (balance - amount >= 0){
+		balance -= amount;
+		return amount;
+	}
+	cout << "Insufficient balance! Rs." << balance << endl; 
+	return 0;
+}
+
+void BankAccount::displayAccountDetails()
+{
+	cout << "Account Number:\t\t" << accountNumber << endl 
+		 << "Account Holder Name:\t" << accountHolderName << endl
+		 << "Current Balance:\tRs." << balance << endl;
+}
+
