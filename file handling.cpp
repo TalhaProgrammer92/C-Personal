b252b96768e969039b824c80335742b9ccc937f0
Talha Ahmad <talha.code92@gmail.com>
2025-02-12 08:45:07 +0500

    Update file handling.cpp

--- file handling.cpp
+++ file handling.cpp
@@ -10,6 +10,9 @@ Classes for File handling:
 Opening a file:
  > By using constructor
  > By using open() member function
+
+Closing a file:
+ > By using close() member function
 */
 
 using namespace std;
@@ -20,11 +23,12 @@ int main()
 	string text = "This is a text file.\nMy name is Talha Ahmad.";
 	ofstream file_o("sample.txt");		// Open the text file
 	file_o << text;						// Writing the 'text' string to the file
+	file_o.close();						// Closing the file
 	
 	// Reading from a file
 	ifstream file_i("sample.txt");		// Open the text file
 	string read;						// A string variale to store data of the file
 	getline(file_i, read);				// Reading from the file and store the data in 'read' variable
+	file_i.close();						// Closing the file
 	cout << read << endl;				// Print the value stored in 'read'
 }
-
